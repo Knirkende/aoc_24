f6ddd7c88a03ea913a3346a17e1856c9c5a9f79b
Ole Holgernes <ole.holgernes@gmail.com>
2024-12-01 09:04:27 +0100

    day1

---
+++ day1/day1.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+with open('input1.txt', 'r') as file:
+    l = file.readlines()
+    left, right = sorted([int(x.split()[0].strip()) for x in l]), sorted([int(x.split()[1].strip()) for x in l])
+
+acc = 0
+right_count = Counter(right)
+
+for i in range(len(left)):
+    #acc = abs(left[i] - right[i]) #part1
+    acc += abs(left[i]) * right_count.get(left[i], 0) #part2
+
+print(acc)
